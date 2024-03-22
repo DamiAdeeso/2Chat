7ab042a1ffc8cdbf69aa70cf3200e23e402bf2e2
DamiAdeeso <damilolaadeeso@gmail.com>
2024-03-22 18:29:45 +0100

    update

---
+++ src/components/InAppNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InAppNav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InAppNav
---
+++ src/pages/MessageList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InAppNav from '../components/InAppNav'
+
+function MessageList() {
+  return (
+    <>
+      <InAppNav/>
+    </>
+  )
+}
+
+export default MessageList
